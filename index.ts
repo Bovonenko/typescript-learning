de3ed380984805f5b1268e62c47559c95f4e03b6
Rostyslav <rostikbovonenko@gmail.com>
2022-12-11 20:14:12 +0100

    readonly

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 interface User {
-	login: string;
+	readonly login: string;
 	password: string;
 	age: number;
 	// addr?: string;
-	addr: string | undefined;
+	readonly addr?: string | undefined;
 	parrents?: {
 		mother?: string;
 		father?: string;
@@ -17,8 +17,28 @@ const user: User = {
 	addr: 'fsdhfjksa',
 };
 
+const userFreeze: Readonly<User> = {
+	login: 'first',
+	password: 'qwerty',
+	age: 50,
+	addr: 'fsdhfjksa',
+};
+userFreeze.age = 4545;
+
+user.login = 'fsdfa';
+
 const dName = '12345';
 
 function sendUserData(obj: User, db?: string): void {
 	console.log(obj.parrents?.father?.toLowerCase, db?.toLocaleLowerCase());
 }
+
+const basicPorts: ReadonlyArray<number> = [3000, 3001, 5555];
+// basicPorts[0] = 5;
+
+basicPorts.push();
+
+// const basicPorts: readonly [number, ...string[]] = [3000, '3001', '5555'];
+// basicPorts[0] = 5;
+
+// basicPorts.push();
