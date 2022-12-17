3ac3c53c6cc0158c277becfcb1d35c2cbb005230
Rostyslav <rostikbovonenko@gmail.com>
2022-12-17 11:30:33 +0100

    request types

--- index.ts
+++ index.ts
@@ -1,52 +1,18 @@
-// let smth: unknown;
+const dataFromControl = {
+	water: 200,
+	el: 350,
+};
 
-// smth = 'str';
+function checkReadings(data: typeof dataFromControl): boolean {
+	const dataFromUser = {
+		water: 200,
+		el: 350,
+	};
 
-// let data: string[] = smth;
-// data.find((e) => e);
-
-// const someValue: unknown = 10;
-// someValue.method();
-
-function fetchData(data: unknown): void {
-	if (typeof data === 'string') {
-		console.log(data.toLocaleLowerCase());
-	}
+	if (data.el === dataFromUser.el && data.water === dataFromUser.water) {
+		return true;
+	} else return false;
 }
 
-const userData = '{"isBirthday": true, "userName": "John", "age": 40}';
-
-function safeParse(s: string): unknown {
-	return JSON.parse(s);
-}
-
-const data = safeParse(userData);
-
-function transferData(d: unknown): void {
-	if (typeof d === 'string') {
-		console.log(d.toLocaleLowerCase());
-	} else if (typeof d === 'object' && d) {
-		console.log(data);
-	} else {
-		console.error('some error');
-	}
-}
-
-transferData(data);
-
-try {
-	if (1) {
-		throw new Error('error');
-	}
-} catch (e) {
-	if (e instanceof Error) {
-		console.log(e.message);
-	} else if (typeof e === 'string') {
-		console.log(e);
-	}
-}
-
-type T0 = any | unknown;
-type T1 = number | unknown;
-type T2 = any & unknown;
-type T3 = number & unknown;
+const PI = 3.14;
+let PIClone: typeof PI;
