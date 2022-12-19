9d0c43cafb93d53ea074b686f286b14e7ef945ab
Rostyslav <rostikbovonenko@gmail.com>
2022-12-19 19:40:57 +0100

    type guard and never switch case

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-var fetchData = function (url, method) {
-    console.log(method);
-};
-var reqOptions = {
-    url: 'https://someurl.com',
-    method: 'GET'
-};
-var str = 'str';
-var method = 'GET';
-fetchData('qqq', 'GET');
-fetchData(reqOptions.url, reqOptions.method);
+function printMsg(msg) {
+    if (Array.isArray(msg)) {
+        msg.forEach(function (m) { return console.log(m); });
+    }
+    else if (typeof msg === 'number') {
+        console.log(msg);
+    }
+    else {
+        console.log(msg);
+    }
+    console.log(msg);
+}
+printMsg(3);
 var box = document.querySelector('.box');
-var input = document.querySelector('input');
-var someNumber = input.value;
-console.log(someNumber * 2);
+box === null || box === void 0 ? void 0 : box.addEventListener('click', function () { });
--- index.ts
+++ index.ts
@@ -1,40 +1,85 @@
-// let num: Number = new Number(5);
-// let num2: number = 5;
-// let num3 = Number(5);
-
-// num = num2;
-// num2 = num;
-
-const num = 5;
-const strNum: string = num.toString();
-const str = '5';
-const numStr: number = +str;
-
-interface Department {
-	name: string;
-	budget: number;
+function printMsg(msg: string[] | number | boolean): void {
+	if (Array.isArray(msg)) {
+		msg.forEach((m) => console.log(m));
+	} else if (isNumber(msg)) {
+		console.log(msg);
+	} else {
+		console.log(msg);
+	}
+	console.log(msg);
 }
 
-const department: Department = {
-	name: 'web-dev',
-	budget: 5000,
-};
+printMsg(3);
 
-interface Project {
-	name: string;
-	projectBudget: number;
+// function isNumber(n: string[] | number | boolean): n is number {
+// 	return typeof n === 'number';
+// }
+
+function isNumber(n: unknown): n is number {
+	return typeof n === 'number';
 }
 
-// const mainProject: Project = {
-// 	...department,
-// 	projectBudget: 5000,
-// };
-
-function transformDepartment(department: Department, amount: number): Project {
-	return {
-		name: department.name,
-		projectBudget: amount,
+interface Car {
+	name: 'car';
+	engine: string;
+	wheels: {
+		number: number;
+		type: string;
 	};
 }
 
-const mainProject = transformDepartment(department, 5000);
+interface Ship {
+	name: 'ship';
+	engine: string;
+	sail: string;
+}
+
+interface Airplane {
+	name: 'airplane';
+	engine: string;
+	wings: string;
+}
+
+interface SuperAirplane {
+	name: 'smth';
+	engine: string;
+	wings: string;
+}
+
+type Vehicle = Car | Ship | Airplane | SuperAirplane;
+
+function repairVehicle(vehicle: Vehicle) {
+	// if (isCar(vehicle)) {
+	// 	vehicle.wheels;
+	// } else if (isShip(vehicle)) {
+	// 	vehicle.sail;
+	// } else {
+	// 	vehicle.wings;
+	// }
+
+	switch (vehicle.name) {
+		case 'car':
+			console.log(vehicle.wheels);
+			break;
+		case 'ship':
+			console.log(vehicle.sail);
+			break;
+		case 'airplane':
+			console.log(vehicle.wings);
+			break;
+		case 'smth':
+			console.log(vehicle.wings);
+			break;
+		default:
+			const smth: never = vehicle;
+			console.log('oops!');
+	}
+}
+
+function isCar(car: Vehicle): car is Car {
+	return (car as Car).wheels.number !== undefined;
+}
+
+function isShip(ship: Vehicle): ship is Ship {
+	return 'sail' in ship;
+}
