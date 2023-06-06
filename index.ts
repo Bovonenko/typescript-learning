3513dbee808e0ec98df82cf169fbb79653cf3606
Rostyslav <rostikbovonenko@gmail.com>
2023-06-06 17:43:04 +0200

    generic Classes

--- index.ts
+++ index.ts
@@ -1,126 +1,29 @@
-// Создать Generic-интерфейс PlayerData, который подходил бы для создания таких объектов:
-// interface Hours {
-// 	total:
-// }
-interface PlayerData<Game, Hours> {
-	game: Game;
-	hours: Hours;
-	server: string;
+class User<T, S> {
+	name: T;
+	age: S;
+
+	constructor(name: T, age: S) {
+		this.name = name;
+		this.age = age;
+	}
+
+	sayMyFullName<T>(surname: T): string {
+		if (typeof surname !== "string") {
+			return `I have only name: ${this.name}`;
+		} else {
+			return `${this.name}, ${surname}`;
+		}
+	}
 }
 
-const player1: PlayerData<string, number> = {
-	game: "CS:GO",
-	hours: 300,
-	server: "basic",
-};
-
-const player2: PlayerData<number, string> = {
-	game: 2048,
-	hours: "300 h.",
-	server: "arcade",
-};
-
-const player3: PlayerData<string, object> = {
-	game: "Chess",
-	hours: {
-		total: 500,
-		inMenu: 50,
-	},
-	server: "chess",
-};
-
-// Массив данных с фигурами содержит объекты, у каждого из которых обязательно есть свойство name
-// Каждый объект может еще содержать дополнительные свойства в случайном виде
-// Свойство name может иметь только 4 варианта
-// Функция calculateAmountOfFigures должна принимать массив с объектами, у которых обязательно должно быть свойство name
-// Возвращает она объект-экземпляр AmountOfFigures
-// Внутри себя подсчитывает сколько каких фигур было в массиве и записывает результаты в AmountOfFigures
-// С текущими данными в консоль должно попадать:
-// { squares: 3, circles: 2, triangles: 2, others: 1 }
-
-enum ENames {
-	Triangle = "triangle",
-	Rect = "rect",
-	Line = "line",
-	Circle = "cicle",
+class AdminUser<T> extends User<string, number> {
+	rules: T;
 }
 
-interface AmountOfFigures {
-	squares: number;
-	circles: number;
-	triangles: number;
-	others: number;
-}
+const ivan = new User("Ivan", 30);
+console.log(ivan.sayMyFullName("Smith"));
 
-interface Figure {
-	name: TNames;
-}
+const nameData = "Alex";
+const ageData = 45;
 
-function calculateAmountOfFigures<T extends Figure>(
-	figure: T[]
-): AmountOfFigures {
-	const amount: AmountOfFigures = figure.reduce(
-		(acc, item) => {
-			switch (item.name) {
-				case "circle":
-					acc.circles += 1;
-					break;
-				case "rect":
-					acc.squares += 1;
-					break;
-				case "triangle":
-					acc.triangles += 1;
-					break;
-				default:
-					acc.others++;
-			}
-			return acc;
-		},
-		{ squares: 0, circles: 0, triangles: 0, others: 0 }
-	);
-	return amount;
-}
-
-type TNames = "triangle" | "rect" | "line" | "circle";
-
-interface IData extends Figure {
-	data?: {
-		[key: string]: number;
-	};
-}
-
-const data: IData[] = [
-	{
-		name: "rect",
-		data: { a: 5, b: 10 },
-	},
-	{
-		name: "rect",
-		data: { a: 6, b: 11 },
-	},
-	{
-		name: "triangle",
-		data: { a: 5, b: 10, c: 14 },
-	},
-	{
-		name: "line",
-		data: { l: 15 },
-	},
-	{
-		name: "circle",
-		data: { r: 10 },
-	},
-	{
-		name: "circle",
-		data: { r: 5 },
-	},
-	{
-		name: "rect",
-		data: { a: 15, b: 7 },
-	},
-	{
-		name: "triangle",
-	},
-];
-
-console.log(calculateAmountOfFigures(data));
+const alex = new User<string, number>(nameData, ageData);
