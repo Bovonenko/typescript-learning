ecadad3158382329248307bfb855b44a588e94b5
Rostyslav <rostikbovonenko@gmail.com>
2023-06-06 14:53:56 +0200

    Generics types and interfaces

--- index.ts
+++ index.ts
@@ -1,41 +1,46 @@
-function processingData<T, S>(data: T[], options: S): string {
-	data.length;
-	switch (typeof data) {
-		case "string":
-			return `${data}, speed ${options}`;
-		case "number":
-			return `${data}, speed ${options}`;
-		default:
-			return "Not valid";
-	}
-}
-
-const res1 = processingData([1], "fast");
-const res2 = processingData(["1"], "slow");
-
-const num = 10;
-
-const res3 = processingData<number, string>([num], "slow");
-
-const processin = <T>(data: T): T => {
-	return data;
-};
-
 interface ProcessingFn {
 	<T>(data: T): T;
 }
 
-let newFunc: ProcessingFn = processin;
-
-interface DataSaver {
-	// processing: typeof processin;
-	processing: ProcessingFn;
+function processin<T>(data: T): T {
+	return data;
 }
 
-const saver: DataSaver = {
-	// processing(data) {
-	// 	console.log(data);
-	// 	return data;
-	// },
-	processing: processin,
+let newFunc: ProcessingFn = processin;
+let fn: <T>(arg: T) => T = <T>(arg: T): T => {
+	return arg;
 };
+
+type Smth<T> = T;
+
+const num: Smth<number> = 5;
+
+interface ParentsOfUser {
+	mother: string;
+	father: string;
+}
+
+interface User<ParentsData extends ParentsOfUser> {
+	login: string;
+	age: number;
+	parents: ParentsData;
+}
+
+const user: User<{ mother: string; father: string; maried: boolean }> = {
+	login: "dfa",
+	age: 2,
+	parents: { mother: "Anna", father: "no data", maried: true },
+};
+
+type OrNull<Type> = Type | null;
+type OneOrMany<Type> = Type | Type[];
+
+const data: OneOrMany<number[]> = [5];
+
+function depositMoney<T extends number | string>(money: T): T {
+	console.log(`req to server with amount: ${money}`);
+	return money;
+}
+
+depositMoney(400);
+depositMoney("400");
