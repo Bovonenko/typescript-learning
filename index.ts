b52c0c585665c0aadaf0ab59e7ac009cb019db8a
Rostyslav <rostikbovonenko@gmail.com>
2023-06-08 14:45:32 +0200

    Template literal types

--- index.ts
+++ index.ts
@@ -1,33 +1,17 @@
 type Currencies = {
 	usa: "usd";
-	china?: "cny";
+	china: "cny";
 	ukraine: "uah";
-	readonly kz: "tenge";
+	kz: "tenge";
 };
 
 type CreateCustomCurr<T> = {
-	-readonly [P in keyof T]-?: string;
+	[P in keyof T as `custom${Capitalize<string & P>}`]: string;
 };
 
 type CustomCurrencies = CreateCustomCurr<Currencies>;
 
-type ROnlyCurr = Readonly<Currencies>;
+type MyAnimation = "fade" | "swipe";
+type Direction = "in" | "out";
 
-// type CustomCurrencies = {
-// 	usa: string;
-// 	china: string;
-// 	ukraine: string;
-// 	kz: string;
-// };
-
-type Keys = "name" | "age" | "role";
-
-type User = {
-	[K in Keys]: string;
-};
-
-const alex: User = {
-	name: "Alex",
-	age: "25",
-	role: "admin",
-};
+type MyNewAnimation = `${MyAnimation}${Capitalize<Direction>}`;
