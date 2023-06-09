45662d9a2d93a15145b5a263a16ca6da34a11a1c
Rostyslav <rostikbovonenko@gmail.com>
2023-06-09 17:54:26 +0200

    abstract classes

--- index.ts
+++ index.ts
@@ -1,55 +1,23 @@
-class Player {
-	private login: string;
-	private _password: string;
-	server: string;
+interface IEngine {
+	model: string;
+	capacity: number;
+	startEngine: (time: Date) => string;
+}
 
-	constructor(login: string) {
-		this.login = login;
-
-		this.logIn = this.logIn.bind(this);
+abstract class AbsractVehicle {
+	model: string;
+	capacity: number;
+	abstract startEngine: (time: Date) => string;
+	stopEngine(time: Date): string {
+		this.startEngine(new Date())
+		return 'Engine stopped'
 	}
+}
 
-	get password() {
-		return this._password;
-	}
-
-	set password(newPass: string) {
-		// validation
-		this._password = newPass;
-	}
-
-	logIn = () => {
-		return `Player ${this.login} online!`;
+class Vehicle extends AbsractVehicle {
+	startEngine = (time: Date) => {
+		return "Started";
 	};
-
-	connect() {
-		// Do smth
-		return this;
-	}
-
-	isPro(): this is CompetitivePlayer {
-		return this instanceof CompetitivePlayer;
-	}
 }
 
-// const test = player.logIn;
-// test();
-
-class CompetitivePlayer extends Player {
-	rank: number;
-
-	checkLogin() {
-		return this.logIn();
-	}
-}
-const player = new Player("kaban4ik");
-console.log(player.connect().logIn());
-
-const player2 = new CompetitivePlayer("kaban");
-console.log(player2.connect().logIn());
-
-const somePlayer: Player | CompetitivePlayer = new CompetitivePlayer("Test3");
-
-somePlayer.isPro() ? console.log(somePlayer) : console.log(somePlayer);
-
-// console.log(player2.checkLogin());
+new Vehicle().
