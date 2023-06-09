8ec4ac11cd2324174c59e2b05f590cbeff723885
Rostyslav <rostikbovonenko@gmail.com>
2023-06-09 11:47:02 +0200

    exercises with utility types

--- index.ts
+++ index.ts
@@ -1,41 +1,141 @@
-type Currencies = {
-	usa: "usd";
-	china: "cny";
-	ukraine: "uah";
-	kz: "tenge";
+// Необходимо типизировать этот большой объект
+// Свойство futureClasses должно быть в зависимости от classes по типу
+// Свойства exClients и futureClients тоже должны быть в зависимости от currClients
+// ИЛИ все три зависят от общего родителя
+
+// Простыми словами: при добавлении свойства в целевой объект они должны быть
+// автоматически добавлены в зависимые (сразу подсказка от TS)
+type ClassesStrings = keyof (typeof fitnessClubCenter)["classes"][number];
+
+interface Class {
+	name: string;
+	startsAt: string;
+	duration: number;
+}
+
+interface FutureClass extends Omit<Class, "startsAt"> {
+	willStartsAt: string;
+}
+
+// type FutureClass = Omit<Class, "startsAt"> & Record<"willStartsAt", string>;
+
+interface IClient {
+	name: string;
+	age: "-" | number;
+	gender: "male" | "female";
+	timeLeft: string;
+	makeCallFor: Date;
+}
+
+type CurrClient = Omit<IClient, "makeCallFor">;
+type ExClient = Omit<IClient, "timeLeft">;
+type FutureClient = Pick<IClient, "name" | "makeCallFor">;
+
+// interface ExClient extends Omit<Client, "timeLeft"> {
+// 	makeCallFor: Date;
+// }
+
+// interface FutureClient extends Pick<Client, "name"> {
+// 	makeCallFor: Date;
+// }
+
+// type ExClient = Omit<Client, "timeLeft"> & Record<"makeCallFor", Date>;
+// type FutureClient = Pick<Client, "name"> & Record<"makeCallFor", Date>;
+
+// interface FutureClass {
+// 	name: Class["name"];
+// 	willStartsAt: Class["startsAt"];
+// 	duration: Class["duration"];
+// }
+
+// interface ExClient {
+// 	name: Client['name'];
+// 	age: Client['age'];
+// 	gender: Client['gender'];
+// 	makeCallFor: Date;
+// }
+
+// interface exClients {
+// 	name: Client['name'];
+// 	age: Client['age'];
+// 	gender: Client['gender'];
+// 	makeCallFor: Date;
+// }
+
+interface IFitnessClubCenter {
+	clubName: string;
+	location: string;
+	classes: Class[];
+	futureClasses: FutureClass[];
+	currClients: CurrClient[];
+	exClients: ExClient[];
+	futureClients: FutureClient[];
+}
+
+const fitnessClubCenter: IFitnessClubCenter = {
+	clubName: "Fitness club Center",
+	location: "central ave. 45, 5th floor",
+	classes: [
+		{
+			name: "yoga",
+			startsAt: "8:00 AM",
+			duration: 60,
+		},
+		{
+			name: "trx",
+			startsAt: "11:00 AM",
+			duration: 45,
+		},
+		{
+			name: "swimming",
+			startsAt: "3:00 PM",
+			duration: 70,
+		},
+	],
+	futureClasses: [
+		{
+			name: "boxing",
+			willStartsAt: "6:00 PM",
+			duration: 40,
+		},
+		{
+			name: "breath training",
+			willStartsAt: "8:00 PM",
+			duration: 30,
+		},
+	],
+	currClients: [
+		{
+			name: "John Smith",
+			age: "-",
+			gender: "male",
+			timeLeft: "1 month",
+		},
+		{
+			name: "Alise Smith",
+			age: 35,
+			gender: "female",
+			timeLeft: "3 month",
+		},
+		{
+			name: "Ann Sonne",
+			age: 24,
+			gender: "female",
+			timeLeft: "5 month",
+		},
+	],
+	exClients: [
+		{
+			name: "Tom Smooth",
+			age: 50,
+			gender: "male",
+			makeCallFor: new Date("2023-08-12"),
+		},
+	],
+	futureClients: [
+		{
+			name: "Maria",
+			makeCallFor: new Date("2023-07-10"),
+		},
+	],
 };
-
-type CurrWithoutUSA = Omit<Currencies, "usa">; // исключение
-type CurrUSAAndUkraine = Pick<Currencies, "ukraine" | "usa">; // filtering
-type CountriesWithoutUSA = Exclude<keyof Currencies, "usa">;
-
-type FadeType = Exclude<MyAnimation, "swipe">; // deleting from union type
-type SwipeType = Extract<MyAnimation | Direction, "swipe">; // select needed type
-
-type CreateCustomCurr<T> = {
-	[P in keyof T as `custom${Capitalize<string & P>}`]: string;
-};
-
-type PlayersNames = "alex" | "john";
-type CustomCurrencies = CreateCustomCurr<Currencies>;
-type GameDataCurr = Record<PlayersNames, CustomCurrencies>;
-
-const gameData: GameDataCurr = {
-	alex: {
-		customChina: "czxv",
-		customUsa: "cxvz",
-		customKz: "vxz",
-		customUkraine: "cvasr",
-	},
-	john: {
-		customChina: "czxv",
-		customUsa: "cxvz",
-		customKz: "vxz",
-		customUkraine: "cvasr",
-	},
-};
-
-type MyAnimation = "fade" | "swipe";
-type Direction = "in" | "out";
-
-type MyNewAnimation = `${MyAnimation}${Capitalize<Direction>}`;
