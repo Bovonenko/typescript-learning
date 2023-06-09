81f059eb139275a2fbb8c7cc72ae8fe88fbd1102
Rostyslav <rostikbovonenko@gmail.com>
2023-06-09 16:47:24 +0200

    protected, private, public

--- index.ts
+++ index.ts
@@ -1,51 +1,41 @@
-class Box {
-	width: number;
-	height: number = 500;
-	volume: number | undefined;
-	_content: string | undefined;
+class Player {
+	private login: string;
+	private _password: string;
+	server: string;
+	protected consent: boolean;
 
-	constructor(width: number, volume?: number, content?: string) {
-		this.width = width;
-		this.volume = volume;
-		this._content = content;
+	get password() {
+		return this._password;
 	}
 
-	calculateVolume(): void {
-		if (!this.volume) {
-			this.volume = this.width * this.height;
-			console.log(`Volume is: ${this.volume}`);
-		} else {
-			console.log(`Volume is: ${this.volume}`);
-		}
-	}
-
-	checkBoxSize(transport: number): string;
-	checkBoxSize(transportes: number[]): string;
-	checkBoxSize(transport: number | number[]): string {
-		if (Array.isArray(transport)) {
-			return transport.some((t) => t >= this.width) ? "Ok" : "not ok";
-		} else {
-			return transport >= this.width ? "Ok" : "not ok";
-		}
-	}
-
-	get boxContent() {
-		return this._content;
-	}
-
-	set boxContent(value) {
-		this._content = value;
-	}
-
-	async content(value: string) {
-		const date = await new Date().toTimeString();
-		this._content = `Date: ${date}, Content: ${value}`;
-		console.log(this._content);
-		// return this._content;
+	set password(newPass: string) {
+		// validation
+		this._password = newPass;
 	}
 }
 
-const firstBox = new Box(250);
-firstBox.volume = 500;
-console.log((firstBox.boxContent = "test"));
-console.log(firstBox.boxContent);
+class CompetitivePlayer extends Player {
+	rank: number;
+
+	isConsented() {
+		this.consent ? "yes" : "no";
+	}
+}
+
+const player = new CompetitivePlayer();
+player.password = "1qaz";
+player.consent; // error
+
+// class User {
+// 	public email: string;
+// 	public name: string;
+
+// 	constructor(email: string, name: string) {
+// 		this.email = email;
+// 		this.name = name;
+// 	}
+// }
+
+// class User1 {
+// 	constructor(public email: string, public name: string) {}
+// }
