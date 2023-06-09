86c3e1394c06385cea03504dbe9027efbe21cf5c
Rostyslav <rostikbovonenko@gmail.com>
2023-06-09 14:42:08 +0200

    overload constructor

--- index.ts
+++ index.ts
@@ -1,14 +1,22 @@
 class Box {
 	width: number;
 	height: number;
+	volume: string;
 
-	constructor(width: number, height: number) {
-		this.width = width;
-		this.height = height;
+	constructor(volume: string);
+	constructor(width: number);
+	constructor(widthOrVolume: number | string) {
+		if (typeof widthOrVolume === "number") {
+			this.width = widthOrVolume;
+		} else {
+			this.volume = widthOrVolume;
+		}
+
+		this.height = 500;
 	}
 }
 
-const firstBox = new Box(250, 30);
+const firstBox = new Box(3);
 console.log(firstBox);
 
 class User {
