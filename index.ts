93ba11137a9d932e8ebb5226f06e69cbbf4c39bf
Rostyslav <rostikbovonenko@gmail.com>
2023-06-09 15:01:53 +0200

    extends, super, override

--- index.ts
+++ index.ts
@@ -1,29 +1,70 @@
 class Box {
 	width: number;
-	height: number;
-	volume: string;
+	height: number = 500;
+	volume: number | undefined;
+	_content: string | undefined;
 
-	constructor(volume: string);
-	constructor(width: number);
-	constructor(widthOrVolume: number | string) {
-		if (typeof widthOrVolume === "number") {
-			this.width = widthOrVolume;
+	constructor(width: number, volume?: number, content?: string) {
+		this.width = width;
+		this.volume = volume;
+		this._content = content;
+	}
+
+	calculateVolume(): void {
+		if (!this.volume) {
+			this.volume = this.width * this.height;
+			console.log(`Volume is: ${this.volume}`);
 		} else {
-			this.volume = widthOrVolume;
+			console.log(`Volume is: ${this.volume}`);
 		}
+	}
 
-		this.height = 500;
+	async content(value: string) {
+		const date = await new Date().toTimeString();
+		this._content = `Date: ${date}, Content: ${value}`;
+		console.log(this._content);
+		// return this._content;
 	}
 }
 
-const firstBox = new Box(3);
+const firstBox = new Box(250);
 console.log(firstBox);
 
-class User {
-	name: string;
+class Styles {
+	[s: string]: string | ((s: string) => boolean);
+
+	method(s: string) {
+		return true;
+	}
 }
 
-const ivan = new User();
-ivan.name = "Ivan";
+const style = new Styles();
+style.color = "red";
+style.font = "roboto";
 
-console.log(ivan);
+class PresentBox extends Box {
+	wrap: string;
+	height: number = 600;
+
+	constructor(wrap: string, width: number) {
+		super(width, 400);
+		this.height = 342;
+		this.wrap = wrap;
+	}
+
+	override async content(value: string, text?: string) {
+		const date = await new Date().toTimeString();
+
+		if (!text) {
+			super.content(value);
+		} else {
+			this._content = `Date: ${date}, Content: ${value}, Text: ${
+				text ? text : "No text"
+			}`;
+		}
+		console.log(this._content);
+		// return this._content;
+	}
+}
+
+const presentBox = new PresentBox("red", 34).content("TV", "Etext");
