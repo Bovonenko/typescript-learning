26e02474b8585c398e3c20a8d416f1dd77f910fd
Rostyslav <rostikbovonenko@gmail.com>
2023-06-10 20:05:18 +0200

    class decorators in TS

--- index.ts
+++ index.ts
@@ -1,23 +1,32 @@
-const myCar = {
-	fuel: "100%",
-	open: true,
-	freeSeats: 3,
+interface ICar {
+	fuel: string;
+	open: boolean;
+	freeSeats: number;
+}
+
+@closeCar
+class myCar implements ICar {
+	fuel: string = "50%";
+	open: boolean = true;
+	freeSeats: number;
 	isOpen() {
 		console.log(this.fuel);
 		return this.open ? "open" : "close";
-	},
-};
-
-function closeCar(car: typeof myCar) {
-	car.open = false;
-	console.log("close car");
-	return car;
+	}
 }
 
-function addFuel(car: typeof myCar) {
-	car.fuel = "100%";
-	console.log("add fuel");
-	return car;
+function closeCar<T extends { new (...arg: any[]): {} }>(constructor: T) {
+	return class extends constructor {
+		open = false;
+	};
 }
 
-addFuel(closeCar(myCar)).isOpen();
+// function addFuel(car: myCar) {
+// 	car.fuel = "100%";
+// 	console.log("add fuel");
+// 	return car;
+// }
+
+const car = new myCar();
+
+console.log(car.isOpen());
